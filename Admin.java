8c6dd9a6e84d7148f8289bee893329990e7e885b
Vitor Oliveira Pacheco <132855102+VitorOliveiraPacheco@users.noreply.github.com>
2023-05-08 21:18:37 -0300

    class admin

---
+++ Admin.java
@@ -0,0 +1,29 @@
+public class Admin extends Cliente {
+
+    private String numRegistro;
+
+    Admin (String numRegistro) {
+
+        this.numRegistro = numRegistro;
+
+    }
+
+    public String getNumRegistro {
+
+        return numRegistro;
+
+    }
+
+    public void setNumRegistro {
+        this.numRegistro=numRegistro;
+    }
+
+    public Cliente[] getListaCliente (){
+        
+        // metódo para listar
+        // os clientes da petshop
+
+    }
+
+
+}
