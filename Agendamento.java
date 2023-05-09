826615a82187c05e14240d076d8677988ffd0e31
pedropillas <130692619+pedropillas@users.noreply.github.com>
2023-05-08 21:12:03 -0300

    Update Agendamento.java

--- Agendamento.java
+++ Agendamento.java
@@ -1,1 +1,28 @@
+import java.sql.Date;
+
+public class Agendamento {
+
+	private Date data;
+	private Date horario;
+	
+	Agendamento (Date data, Date horario) {
+		this.data = data;
+		this.horario = horario;
+	}
+	
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public Date getHorario() {
+		return horario;
+	}
+	public void setHorario(Date horario) {
+		this.horario = horario;
+	}
+	
+	
+}
 
