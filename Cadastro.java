b511bc3239c212baf7d678f7b5750113297bc1a1
JenisonSchmitt <75100716+JenisonSchmitt@users.noreply.github.com>
2023-05-23 17:30:50 -0300

    Create Cadastro.java

---
+++ Cadastro.java
@@ -0,0 +1,52 @@
+package A3;
+
+public class Cadastro {
+
+    private String nome;
+    private String endereco;
+    private String email;
+    private String telefone;
+
+    Cadastro (){
+    }
+    Cadastro (String nome, String endereco, String email, String telefone, String cpf) {
+    	super();
+        this.nome = nome;
+        this.endereco = endereco;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public String getNome () {
+        return nome;
+    }
+
+    public void setNome (String nome) {
+        this.nome = nome;
+    }
+
+        public String getEndereco () {
+        return endereco;
+    }
+
+    public void setEndereco (String endereco) {
+        this.endereco = endereco;
+    }
+
+        public String getEmail () {
+        return email;
+    }
+
+    public void setEmail (String email) {
+        this.email = email;
+    }
+
+        public String getTelefone () {
+        return telefone;
+    }
+
+    public void setTelefone (String telefone) {
+        this.telefone = telefone;
+    }
+
+}
