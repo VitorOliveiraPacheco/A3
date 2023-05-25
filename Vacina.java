23267b00f71702854eda237dbb1a98a75ca56a95
JenisonSchmitt <75100716+JenisonSchmitt@users.noreply.github.com>
2023-05-25 15:31:11 -0300

    Update Vacina.java

--- Vacina.java
+++ Vacina.java
@@ -1,48 +1,38 @@
 package A3;
 
-import javax.swing.JOptionPane;
-
 public class Vacina extends AgendamentoVeterinario {
 
-	private String tipovacina;
-	private int vlrconsulta = 50;
-	private int vlrpolivalente = 80;
-	private int vlrraiva = 50;
-	private int vlrV= 80;
+	private String vacina;
+	private int tipovacina;
+	private int valorvacina;
 	
 	Vacina(){
 	}
-	Vacina (String tipovacina, String diavet, String tipoconsulta, int vlrconsulta, int vlrpolivalente, int vlrraiva, int vlrV) {
+	Vacina (String vacina,int tipovacina, String diavet, int tipoconsulta,int valorvacina) {
 		super(tipoconsulta, diavet, tipoconsulta);
 		this.tipovacina= tipovacina;
-		this.vlrconsulta = vlrconsulta;
-		this.vlrpolivalente = vlrpolivalente;
-		this.vlrraiva = vlrraiva;
-		this.vlrV= vlrV;
+		this.valorvacina = valorvacina;
+		this.vacina= vacina;
 	}
 
-	public String getTipo() {
+	public String getvacina() {
+		return vacina;
+	}
+	public void setvacina(String vacina) {
+		this.vacina = vacina;
+	}
+	public int getvalorvacina() {
+		return valorvacina;
+	}
+	public void setvalorvacina(int valorvacina) {
+		this.valorvacina = valorvacina;
+	}
+	public int gettipovacina() {
 		return tipovacina;
 	}
 
-	public void setTipo(String tipovacina) {
+	public void settipovacina(int tipovacina) {
 		this.tipovacina = tipovacina;
-	}
-	
-	public void valorconsulta() {
-		JOptionPane.showMessageDialog(null, "O valor total ficará: \n R$ " + this.vlrconsulta + ",00! ");
-	}
-	
-	public void valorpolivanteca() {
-		JOptionPane.showMessageDialog(null, "O valor total ficará: \n R$ " + (this.vlrconsulta + this.vlrpolivalente)+ ",00! ");
-	}
-	
-	public void valorraiva() {
-		JOptionPane.showMessageDialog(null, "O valor total ficará: \n R$ " + (this.vlrconsulta + this.vlrraiva)+ ",00! ");
-	}
-	
-	public void valorV() {
-		JOptionPane.showMessageDialog(null, "O valor total ficará: \n R$ " + (this.vlrconsulta + this.vlrV)+ ",00! ");
 	}
 	
 }
