3d5b6b8f1306809a3518e7ef97b1abd7a92d8244
JenisonSchmitt <75100716+JenisonSchmitt@users.noreply.github.com>
2023-05-25 15:32:49 -0300

    Update A3.sql

--- A3.sql
+++ A3.sql
@@ -36,33 +36,45 @@ dia DATE primary key not null unique,
 17HR varchar (50)
 );
 
+create table administrador(
+numreg int primary key not null unique,
+nome varchar (30)
+);
+
 insert into agendVET (dia)  values (STR_TO_DATE('03/07/2023','%d/%m/%Y'));
 insert into agendVET (dia)  values (STR_TO_DATE('04/07/2023','%d/%m/%Y'));
 insert into agendVET (dia)  values (STR_TO_DATE('05/07/2023','%d/%m/%Y'));
 insert into agendVET (dia)  values (STR_TO_DATE('06/07/2023','%d/%m/%Y'));
-update agendVET set 09HR = 'Ozzy' where dia = '2023-07-04';
-update agendVET set 09HR = 'Bruce' where dia = '2023-07-05';
-update agendVET set 10HR = 'Theo' where dia = '2023-07-05';
-update agendVET set 11HR = 'Mike' where dia = '2023-07-05';
-update agendVET set 09HR = 'Simba' where dia = '2023-07-06';
+update agendVET set 09HR = 'Ozzy- Com Vacina' where dia = '2023-07-04';
+update agendVET set 09HR = 'Bruce- Sem Vacina' where dia = '2023-07-05';
+update agendVET set 10HR = 'Theo- Sem Vacina' where dia = '2023-07-05';
+update agendVET set 11HR = 'Mike- Com Vacina' where dia = '2023-07-05';
+update agendVET set 09HR = 'Simba- Com Vacina' where dia = '2023-07-06';
 
 insert into agendPET (dia)  values (STR_TO_DATE('05/07/2023','%d/%m/%Y'));
 insert into agendPET (dia)  values (STR_TO_DATE('06/07/2023','%d/%m/%Y'));
 insert into agendPET (dia)  values (STR_TO_DATE('07/07/2023','%d/%m/%Y'));
-update agendPET set 16HR = 'Zeca' where dia = '2023-07-05';
-update agendPET set 13HR = 'Buddy' where dia = '2023-07-06';
-update agendPET set 17HR = 'Frederico' where dia = '2023-07-06';
-update agendPET set 14HR = 'Bidu' where dia = '2023-07-07';
+update agendPET set 16HR = 'Zeca- Com Tosa' where dia = '2023-07-05';
+update agendPET set 13HR = 'Buddy- Com Tosa' where dia = '2023-07-06';
+update agendPET set 17HR = 'Frederico- Sem Tosa' where dia = '2023-07-06';
+update agendPET set 14HR = 'Bidu- Com Tosa' where dia = '2023-07-07';
 update agendPET set 15HR = 'Scooby' where dia = '2023-07-07';
-update agendPET set 16HR = 'Lord' where dia = '2023-07-07';
-update agendPET set 17HR = 'Apolo' where dia = '2023-07-07';
+update agendPET set 16HR = 'Lord- Sem Tosa' where dia = '2023-07-07';
+update agendPET set 17HR = 'Apolo- Com Tosa' where dia = '2023-07-07';
 
-
+insert into administrador (numreg, nome) values (1, 'Jenison');
+insert into administrador (numreg, nome) values (2, 'Katrine');
+insert into administrador (numreg, nome) values (3, 'Manuela');
+insert into administrador (numreg, nome) values (4, 'Pedro');
+insert into administrador (numreg, nome) values (5, 'Richard');
+insert into administrador (numreg, nome) values (6, 'Thaynara');
+insert into administrador (numreg, nome) values (7, 'Vitor');
 
 Select * from animal;
 SELECT * FROM cliente;
-select * from agendVET;		
+select * from agendVET;
 select * from agendPET;
+select * from administrador;
 
 
 
