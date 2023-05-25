bb3146c40be1918a85460ff93c8a9eed683db604
JenisonSchmitt <75100716+JenisonSchmitt@users.noreply.github.com>
2023-05-25 15:28:58 -0300

    Update Cadastro.java

--- Cadastro.java
+++ Cadastro.java
@@ -16,15 +16,12 @@ public class Cadastro {
         this.email = email;
         this.telefone = telefone;
     }
-
     public String getNome () {
         return nome;
     }
-
     public void setNome (String nome) {
         this.nome = nome;
     }
-
         public String getEndereco () {
         return endereco;
     }
@@ -32,21 +29,16 @@ public class Cadastro {
     public void setEndereco (String endereco) {
         this.endereco = endereco;
     }
-
         public String getEmail () {
         return email;
     }
-
     public void setEmail (String email) {
         this.email = email;
     }
-
         public String getTelefone () {
         return telefone;
     }
-
     public void setTelefone (String telefone) {
         this.telefone = telefone;
     }
-
 }
