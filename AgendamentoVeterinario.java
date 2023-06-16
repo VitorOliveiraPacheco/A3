880a663776cc6253e336cdf6d8fae874e8a5cb35
JenisonSchmitt <75100716+JenisonSchmitt@users.noreply.github.com>
2023-06-16 15:55:28 -0300

    Update AgendamentoVeterinario.java

--- AgendamentoVeterinario.java
+++ AgendamentoVeterinario.java
@@ -1,26 +1,35 @@
 package A3;
 
 public class AgendamentoVeterinario {
-	private int tipoconsulta;
+	
+	private int valorconsulta;
 	private String diavet;
+	private String horavet;
 	
 	AgendamentoVeterinario(){
 	}
-	AgendamentoVeterinario(int tipoconsulta, String diavet, int tipovacina){
-		this.tipoconsulta= tipoconsulta;
+	AgendamentoVeterinario(int valorconsulta, String diavet, String horavet){
+		this.valorconsulta= valorconsulta;
 		this.diavet= diavet;
+		this.horavet= horavet;
 	}
 
-	public int getTipoconsulta() {
-		return tipoconsulta;
+	public int getvalorconsulta() {
+		return valorconsulta;
 	}
-	public void setTipoconsulta(int tipoconsulta) {
-		this.tipoconsulta = tipoconsulta;
+	public void setvalorconsulta(int valorconsulta) {
+		this.valorconsulta = valorconsulta;
 	}
 	public String getdiavet() {
 		return diavet;
 	}
 	public void setdiavet(String diavet) {
 		this.diavet = diavet;
+	}
+	public String gethoravet() {
+		return horavet;
+	}
+	public void sethoravet(String horavet) {
+		this.horavet = horavet;
 	}
 }
